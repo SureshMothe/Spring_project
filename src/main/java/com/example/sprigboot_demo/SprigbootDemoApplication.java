ac7f52b36d12233f5a79440e29791f8285111bc1
SureshMothe <58295209+SureshMothe@users.noreply.github.com>
2023-05-13 14:27:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/sprigboot_demo/SprigbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprigboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprigboot_demo/SprigbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprigboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
